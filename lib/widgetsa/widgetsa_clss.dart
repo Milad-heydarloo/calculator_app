665e3b0a7e09e2eafb07e4d94531369b669e3b17
milad <78633834+Milad-heydarloo@users.noreply.github.com>
2023-07-22 06:33:24 -0700

    TextButton.styleFrom & shape: CircleBorder & side: BorderSide(               width: 1,             )

--- lib/widgetsa/widgetsa_clss.dart
+++ lib/widgetsa/widgetsa_clss.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 Widget getbody(String t1, String t2, String t3, String t4) {
@@ -5,43 +6,64 @@ Widget getbody(String t1, String t2, String t3, String t4) {
     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
     children: [
       TextButton(
+        style: TextButton.styleFrom(
+            shape: CircleBorder(side: BorderSide(width: 1))),
         onPressed: () {},
         child: Padding(
           padding: EdgeInsets.all(5),
           child: Text(
+            textAlign: TextAlign.center,
             '$t1',
-            style:
-                TextStyle(fontSize: 20, color: Colors.white, fontFamily: 'dm'),
+            style: TextStyle(fontSize: 20, color: Colors.white),
           ),
         ),
-      ), TextButton(
+      ),
+      TextButton(
+        style: TextButton.styleFrom(
+            shape: CircleBorder(side: BorderSide(width: 1))),
         onPressed: () {},
         child: Padding(
           padding: EdgeInsets.all(5),
           child: Text(
+            textAlign: TextAlign.center,
             '$t2',
-            style:
-                TextStyle(fontSize: 20, color: Colors.white, fontFamily: 'dm'),
+            style: TextStyle(fontSize: 20, color: Colors.white),
           ),
         ),
-      ), TextButton(
+      ),
+      TextButton(
+        style: TextButton.styleFrom(
+          shape: CircleBorder(
+            side: BorderSide(
+              width: 1,
+            ),
+          ),
+        ),
         onPressed: () {},
         child: Padding(
-          padding: EdgeInsets.all(5),
+          padding: EdgeInsets.all(
+            5,
+          ),
           child: Text(
+            textAlign: TextAlign.center,
             '$t3',
-            style:
-                TextStyle(fontSize: 20, color: Colors.white, fontFamily: 'dm'),
+            style: TextStyle(fontSize: 20, color: Colors.white),
           ),
         ),
-      ), TextButton(
+      ),
+      TextButton(
+        style: TextButton.styleFrom(
+          shape: CircleBorder(
+            side: BorderSide(width: 1),
+          ),
+        ),
         onPressed: () {},
         child: Padding(
           padding: EdgeInsets.all(5),
           child: Text(
+            textAlign: TextAlign.center,
             '$t4',
-            style:
-                TextStyle(fontSize: 20, color: Colors.white, fontFamily: 'dm'),
+            style: TextStyle(fontSize: 20, color: Colors.white),
           ),
         ),
       ),
