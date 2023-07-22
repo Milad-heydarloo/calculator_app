2e8a0c829fe9b100bdf9b3453d60e9288658eb5c
milad <78633834+Milad-heydarloo@users.noreply.github.com>
2023-07-22 07:23:06 -0700

    get text color operator

--- lib/widgetsa/widgetsa_clss.dart
+++ lib/widgetsa/widgetsa_clss.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 Color get_body_but(String txt) {
   //inja age text shabih on list bashe
   //amber bar migardeh nabashe abi
@@ -8,6 +9,7 @@ Color get_body_but(String txt) {
   }
   return Colors.blue;
 }
+
 //in method ye vorodi migireh az jaii ke seda mishe
 //to list check mikoneh age bashe
 //true bar migardoneh
@@ -20,49 +22,59 @@ bool is_operatour(String txt) {
   }
   return false;
 }
+//get text color operator
+Color get_text_color(String tx) {
+  if (is_operatour(tx)) {
+    return Colors.black;
+  }
+  return Colors.white;
+}
+
 Widget getbody(String t1, String t2, String t3, String t4) {
   return Row(
     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
     children: [
       TextButton(
         style: TextButton.styleFrom(
-          backgroundColor: get_body_but('$t1'),
-
-            shape: CircleBorder(side: BorderSide(width: 2,color: Colors.white))),
+            backgroundColor: get_body_but('$t1'),
+            shape:
+                CircleBorder(side: BorderSide(width: 2, color: Colors.white))),
         onPressed: () {},
         child: Padding(
           padding: EdgeInsets.all(5),
           child: Text(
             textAlign: TextAlign.center,
             '$t1',
-            style: TextStyle(fontSize: 20, color: Colors.white),
+            style: TextStyle(
+              fontSize: 20,
+              color: get_text_color('$t1'),
+            ),
           ),
         ),
       ),
       TextButton(
-
         style: TextButton.styleFrom(
             backgroundColor: get_body_but('$t2'),
-            shape: CircleBorder(side: BorderSide(width: 2,color: Colors.white))),
+            shape:
+                CircleBorder(side: BorderSide(width: 2, color: Colors.white))),
         onPressed: () {},
         child: Padding(
           padding: EdgeInsets.all(5),
           child: Text(
             textAlign: TextAlign.center,
             '$t2',
-            style: TextStyle(fontSize: 20, color: Colors.white),
+            style: TextStyle(
+              fontSize: 20,
+              color: get_text_color('$t2'),
+            ),
           ),
         ),
       ),
       TextButton(
         style: TextButton.styleFrom(
-
           backgroundColor: get_body_but('$t3'),
           shape: CircleBorder(
-            side: BorderSide(
-              width: 2,
-              color: Colors.white
-            ),
+            side: BorderSide(width: 2, color: Colors.white),
           ),
         ),
         onPressed: () {},
@@ -73,7 +85,10 @@ Widget getbody(String t1, String t2, String t3, String t4) {
           child: Text(
             textAlign: TextAlign.center,
             '$t3',
-            style: TextStyle(fontSize: 20, color: Colors.white),
+            style: TextStyle(
+              fontSize: 20,
+              color: get_text_color('$t3'),
+            ),
           ),
         ),
       ),
@@ -81,7 +96,7 @@ Widget getbody(String t1, String t2, String t3, String t4) {
         style: TextButton.styleFrom(
           backgroundColor: get_body_but('$t4'),
           shape: CircleBorder(
-            side: BorderSide(width: 2,color: Colors.white),
+            side: BorderSide(width: 2, color: Colors.white),
           ),
         ),
         onPressed: () {},
@@ -90,7 +105,10 @@ Widget getbody(String t1, String t2, String t3, String t4) {
           child: Text(
             textAlign: TextAlign.center,
             '$t4',
-            style: TextStyle(fontSize: 20, color: Colors.white),
+            style: TextStyle(
+              fontSize: 20,
+              color: get_text_color('$t4'),
+            ),
           ),
         ),
       ),
